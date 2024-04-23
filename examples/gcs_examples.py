8e4b1a92e9f3e514a48373aa86d1c639468e703b
Amelia Pessoa <m.amelia.pessoa@gmail.com>
2024-04-23 19:28:29 -0300

    fix

--- examples/gcs_examples.py
+++ examples/gcs_examples.py
@@ -7,12 +7,12 @@ STORAGE_TYPE = 'GCS'
 
 
 # Autenticação
-repository = "lab-xpto"
-project_id = "labgooru"
-client_id = "116854079973672440157"
-client_email = "airflow-bucket@labgooru.iam.gserviceaccount.com"
-private_key = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDFMAVKcyBJsg7J\nPLyf8rpsjW1cymzpj4+TDpB0/CAj3Lhc8A9mf6BvfM7verczAkr0ZsQBKHxDHN7s\n9qWZguQuKHgQI6OsRzmwYk5myCHm0BVO6bIvfcbbrnSy/yKkjZwY/A8LTNtC1m0N\nf/cm8wKT5PFI095yjiGEapcWt1zyIlvrwDyhNnXwV1CXY/QgjTxwEUdQLALJHpjC\nRvApI5xlgFbsocDQ8Niek26zhmeIvBc/b2KKG8Zu/AWdKWLz6xKd6DOHTAx0KWyh\n7Rxey+d85Jr8ArC+iMUFUwhEr0sHiu1a7fwbu0ZxlxSNxV8rilC5THTHlMlenOS5\nIQhYMk5LAgMBAAECggEAXHlROeF0xmrdGU3FDePIvqiynX4HBp9/TxFY+KvWrMrR\noTApoMGArVgkOdXcfWz5VtWs3PguElFDUHC5J51lQipTaAo2s0/xw4DO0M/Rakuy\neoAJQtFjBVoyu1EAX5hTmLn4mi2QdC9YFCkrcJYtYstOxn8pxqJWIKHWOl6ClcBo\nuJKgkd1C2UmW8HvifH/sbOBCY/SxQGq2OsxnD3SWbfNw0f+xPlf3hwd+4PLZE0qa\n44w7H+YFYA/1+7b6TlW8g4S2YXaAgDxXTldA2QFlM9sfGZgfqN3AZcUplULajfaF\nE9sk0/BsJxC00NJqNCE2rkUNCK5F6fLfvA8pSIELvQKBgQD1eyGH9nG3Vp+XPAz+\nnF/PpZs9N3+6YFXcIYNBE2fSbpS7cibgioJ4wPbS8r4WKdjltjbR0D3oscJORJsM\nVxfF5DRpQjmRTm+2DM6ixEDCiRXnjHG1yEac2gFa3vRq7+zO+K3CzNMa5BDuu/Ea\n/fS1lVeUfSNcuqo3HkMvHCXJVQKBgQDNox9TwSndqQ+Z/NcFCgPYYAW099VoVLGk\ncHcaxdL+32Dcu0H0gLXRG6t141fsxFiusCwi7UIDL/1D4u7EhDYu92vZk+pFaqTs\nNKbOD8lCVh7uGiaOvxbhGkKLRb/w8KO8jUU4T+q2yHW6MOhJK9G6rgw6sBev0LDs\noDGxkp85HwKBgQCHtrMq/8yOl41ThxeIC9vMaLoSdgRffbL6KGzwJVJYvFnt54Ym\nzWykZcoPhbQhfLE1Di/wfzg61UufCb7Oa7fw5+Ex8DLzanHIK/xxcB59blx1zudu\noHKpdL0bB/gIxuwc4M7vy11KmJvj4HPDgHMxkIcCyMwsD+ba4hgyi1U4oQKBgQC3\nMjiJdA+pIqD8jWy9V4O/cyQCabwcWz97AJqLJmvnlfeEDesfOL9BkEX7G1NMYkuj\nLN3VK8tgbZNStEwElMh0pQXW61iNCQnuSKXF8/hXecPKWU+6YfCvD6byzmvF6Yvl\nQXHGTiQLPKDtA/8cmMYaak4IxrIDdob392ruCHKC/wKBgG5CjXANHcFpGBh9ALnt\nruEA3bfSzVJ9rGxHOc7WrTWcgw4n24fAFmZB4iIZGjLJGdKJljFE/oTTNYQPZo7q\npshxbq6vYih+ju7n7HJjEX+qN0RcT1TgufuGo+J7PmS52vwRUY9LS1JtWpbjt3TB\nnNenNpH2dJRgsJIoQhs5bxY2\n-----END PRIVATE KEY-----\n"
-private_key_id = "6ce7192961cd923fd590264ff53f7ff85716e213"
+repository = "my-repository"
+project_id = "project_id"
+client_id = "client_id"
+client_email = "client_email"
+private_key = "private_key"
+private_key_id = "private_key_id"
 
 # Cria um objeto de autenticação e define as credenciais
 auth = Auth(STORAGE_TYPE).authenticator
